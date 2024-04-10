005589e091b1fa7751d01b52b5cac577196182d3
jazzyj1867 <136993666+jazzyj1867@users.noreply.github.com>
2024-04-10 03:59:15 +0000

    checking file

---
+++ P1HW1FrownerJasmine.py
@@ -0,0 +1,13 @@
+#FrownerJasmine
+#04/09/2024
+#P1HW1 
+#write code that collects information from user, processes information collected and display results to user.
+print('Enter integer:')
+integer = int(input())
+print('You entered:', integer)
+print(integer,'squared is', integer * integer)
+print('And', integer, 'is cubed', integer * integer * integer, '!!')
+print('Enter another integer:')
+newinteger = int(input())
+print(integer, '+', newinteger, 'is', integer + newinteger,) 
+print(integer, '*', newinteger, 'is', integer * newinteger,)
