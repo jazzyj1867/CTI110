64e7b49b06fb9c48a9b541ce999f4b606bfc6c5f
jazzyj1867 <136993666+jazzyj1867@users.noreply.github.com>
2024-04-12 15:58:51 +0000

    Homework

---
+++ P2Lab1FrownerJasmine.py
@@ -0,0 +1,13 @@
+#FrownerJasmine
+#04/12/2024
+#P2LAB1 
+# write code that performs mathematical calculations and displays information to users
+
+print(f"What is the radius of the circle?") 
+radius = float(input())
+diameter = radius * 2
+print(f"The diameter of the circle is {diameter: .1f}")
+circumference = diameter * 3.1416
+print(f"The circumference of the circle is {circumference: .2f} ")
+area = 3.1416* radius**2
+print(f"The area of the circle is {area: .4f} ")
