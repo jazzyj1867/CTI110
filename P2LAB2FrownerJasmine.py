80742378a2ade683be8ed7dd4056e57f9ad2cfa9
jazzyj1867 <136993666+jazzyj1867@users.noreply.github.com>
2024-04-12 17:16:14 +0000

    updated

---
+++ P2LAB2FrownerJasmine.py
@@ -0,0 +1,7 @@
+#FrownerJasmine
+#04/12/2024
+#PLAB2
+#write code that uses a dictionary to store user input and displays output to the user
+
+keys = {'Camaro': 1821, 'Prius': 5236, 'Model S': 110, 'Silverado': 26,}
+print (f"The Keys in the Dictonary are: {keys}")
--- P2Lab1FrownerJasmine.py
+++ P2Lab1FrownerJasmine.py
@@ -3,11 +3,14 @@
 #P2LAB1 
 # write code that performs mathematical calculations and displays information to users
 
-print(f"What is the radius of the circle?") 
-radius = float(input())
+#Input Radius
+radius = float(input(f"What is the radius of the circle?")) 
+#math used to do calucations
+import math
+circumference = 2 * math.pi* radius
 diameter = radius * 2
+area = math.pi* radius**2
+#Show Results
 print(f"The diameter of the circle is {diameter: .1f}")
-circumference = diameter * 3.1416
 print(f"The circumference of the circle is {circumference: .2f} ")
-area = 3.1416* radius**2
-print(f"The area of the circle is {area: .4f} ")
+print(f"The area of the circle is {area: .3f} ")
