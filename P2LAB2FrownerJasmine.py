bb4899c6191ee1fdce8fd413b1699044ff8b9181
jazzyj1867 <136993666+jazzyj1867@users.noreply.github.com>
2024-04-12 17:49:02 +0000

    homework2

--- P2LAB2FrownerJasmine.py
+++ P2LAB2FrownerJasmine.py
@@ -3,5 +3,26 @@
 #PLAB2
 #write code that uses a dictionary to store user input and displays output to the user
 
-keys = {'Camaro': 1821, 'Prius': 5236, 'Model S': 110, 'Silverado': 26,}
-print (f"The Keys in the Dictonary are: {keys}")
+#Dictonary
+cars = {'Camaro': 1821, 'Prius': 52.36, 'Model S': 110, 'Silverado': 26,}
+#get keys from dict
+cars_keys = cars.keys()
+print(cars_keys)
+
+print(*cars_keys, sep = ", ")
+
+#enter car
+car_name = input("Enter a car:")
+
+#Get MGP
+car_mpg = cars[car_name]
+print(f"The {car_name} gets {car_mpg} miles per gallon")
+
+#get miles from user
+miles_driven = float(input(f"How many miles will you drive the {car_name}?"))
+
+#calcuate
+gallons_needed = miles_driven/car_mpg
+
+#display with F
+print(f"{gallons_needed:.2f} gallons of gas are needed {car_name} {miles_driven}")
